80ae9563c5e05059699ee9bc524a2b88de52dbcb
Chris Shea <cmshea@gmail.com>
2014-03-04 13:17:09 -0500

    Prep 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject turbovote.imbarcode "0.1.3"
+(defproject turbovote.imbarcode "0.1.4-SNAPSHOT"
   :description "Generate USPS Intelligent Mail Barcodes"
   :url "https://github.com/turbovote/imbarcode"
   :license {:name "Eclipse Public License"
