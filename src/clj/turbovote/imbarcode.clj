c7d4d4c61ec8d2a8cbdca136294dafa58fecba62
Troy Hill <thill@snowfall.local>
2015-02-04 14:12:27 -0700

    [TH] add a single arity encode option to take full digits format

--- src/clj/turbovote/imbarcode.clj
+++ src/clj/turbovote/imbarcode.clj
@@ -18,36 +18,6 @@
 (def ^:export service-type-id:destination "040")
 (def ^:export service-type-id:origin "050")
 
-(defn ^:export encode
-  "Generate the texual representation of an encoded USPS IMbarcode
-   from the data provided. For origin tracing, pass in a barcode id,
-   service type id, customer number, and routing number.  For
-   destination tracing, pass in a barcode id, service type id, mailer
-   id, serial number, and routing number.
-
-   The generated string will consist of the characters [ADFT].  See
-   section 3.1.3 of the IMb spec for a detailed description of the
-   parameters."
-  ([barcode service customer-number routing]
-     {:pre [(every? string? [barcode service customer-number routing])
-            (= (count barcode) 2)
-            (= (count service) 3)
-            (= (count customer-number) 15)
-            (#{0 5 9 11} (count routing))]}
-     (encode-binary-data
-      (binary-encode barcode service customer-number routing)))
-  ([barcode service mailer serial-number routing]
-     {:pre [(every? string? [barcode service mailer serial-number routing])
-            (= (count barcode) 2)
-            (= (count service) 3)
-            (or (and (= (count mailer) 6)
-                     (= (count serial-number) 9))
-                (and (= (count mailer) 9)
-                     (= (count serial-number) 6)))
-            (#{0 5 9 11} (count routing))]}
-     (encode-binary-data
-      (binary-encode barcode service mailer serial-number routing))))
-
 (def origin-service-types #{"050" "051" "052"})
 
 (defn ^:export split-structure-digits
@@ -67,3 +37,50 @@
                            :serial-number (subs mailer-and-serial 6)}
           :9-digit-mailer {:mailer-id (subs mailer-and-serial 0 9)
                            :serial-number (subs mailer-and-serial 9)})))))
+
+(defn ^:export encode
+  "Generate the texual representation of an encoded USPS IMbarcode
+   from the data provided. For origin tracing, pass in a barcode id,
+   service type id, customer number, and routing number.  For
+   destination tracing, pass in a barcode id, service type id, mailer
+   id, serial number, and routing number.
+
+   The generated string will consist of the characters [ADFT].  See
+   section 3.1.3 of the IMb spec for a detailed description of the
+   parameters."
+  ([imbarcode]
+     {:pre [(string? imbarcode)
+            (not (nil? (re-find #"\A\d*\z" imbarcode)))
+            (<= 20 (count imbarcode))
+            (>= 31 (count imbarcode))]}
+     (let [{:keys [barcode service routing] :as imb-data}
+            (split-structure-digits imbarcode)]
+       (if (contains? origin-service-types service)
+         (encode barcode
+                 service
+                 (:customer-number imb-data)
+                 routing)
+         (encode barcode
+                 service
+                 (get-in imb-data [:6-digit-mailer :mailer-id])
+                 (get-in imb-data [:6-digit-mailer :serial-number])
+                 routing))))
+  ([barcode service customer-number routing]
+     {:pre [(every? string? [barcode service customer-number routing])
+            (= (count barcode) 2)
+            (= (count service) 3)
+            (= (count customer-number) 15)
+            (#{0 5 9 11} (count routing))]}
+     (encode-binary-data
+      (binary-encode barcode service customer-number routing)))
+  ([barcode service mailer serial-number routing]
+     {:pre [(every? string? [barcode service mailer serial-number routing])
+            (= (count barcode) 2)
+            (= (count service) 3)
+            (or (and (= (count mailer) 6)
+                     (= (count serial-number) 9))
+                (and (= (count mailer) 9)
+                     (= (count serial-number) 6)))
+            (#{0 5 9 11} (count routing))]}
+     (encode-binary-data
+      (binary-encode barcode service mailer serial-number routing))))
