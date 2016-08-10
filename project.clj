7c6206e454ed81d3c8421c7d006e960f6fc29337
Wes Morgan <wesmorgan@icloud.com>
2016-08-10 13:45:37 -0600

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/imbarcode "1.0.0"
+(defproject democracyworks/imbarcode "1.0.1-SNAPSHOT"
   :description "Generate USPS Intelligent Mail Barcodes"
   :url "https://github.com/democracyworks/imbarcode"
   :license {:name "Eclipse Public License"
